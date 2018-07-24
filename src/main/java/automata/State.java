e23942d23cb1fd0a424dea58613a1fbbfc99ad2f
duckly <duckly_lee@163.com>
2018-07-24 16:46:51 +0800

    add license header

--- src/main/java/automata/State.java
+++ src/main/java/automata/State.java
@@ -1,6 +1,6 @@
 /*
  * Written by Yong Li (liyong@ios.ac.cn)
- * This file is part of the Buchi which is a simple version of SemiBuchi.
+ * This file is part of the Buchi.
  * 
  * Buchi is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Lesser General Public License as published
